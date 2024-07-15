0843e2d959724b067094a90cc256b2c415f5a2a6
Dmitrii Zorin <zchakiz@gmail.com>
2024-07-15 11:00:17 +0300

    Initial Commit

---
+++ TestTaskForIncetro/TestTaskForIncetroApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestTaskForIncetroApp.swift
+//  TestTaskForIncetro
+//
+//  Created by Zorin Dmitrii on 15.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct TestTaskForIncetroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
